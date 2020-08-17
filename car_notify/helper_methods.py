5c532f6e683044566254bbf054cec9e266b7937a
emiride <emir.hodzich@gmail.com>
2020-08-17 17:13:14 +0200

    almost ready app

---
+++ car_notify/helper_methods.py
@@ -0,0 +1,47 @@
+import requests
+from lxml import html
+import math
+import smtplib
+
+def __get_number_of_results(url):
+    r = requests.get(url+"1")
+    tree = html.fromstring(r.text)
+    num_of_results = tree.xpath(".//div[@class='brojrezultata']/span")[0]
+    return int(num_of_results.text)
+
+def get_number_of_pages(url):
+    num_of_results = __get_number_of_results(url)
+    return math.ceil(num_of_results/30)+1
+
+def get_cars_urls(url):
+    r = requests.get(url)
+    tree = html.fromstring(r.text)
+    titles = tree.xpath(".//div[@class='naslov']/a")
+    car_urls = []
+    for title in titles:
+        car_urls.append(title.attrib.get("href"))
+    return car_urls
+
+def send_email(user, pwd, recipient, subject, body):
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print("Successfully sent the mail")
+    except:
+        print("Failed to send mail")
+
+def get_email_body(new_urls_to_mail, deleted_urls_to_mail):
+    new_line = '\n'
+    return f"Nova auta po pretrazi:{new_line}{new_line.join(new_urls_to_mail)}{new_line}{new_line}Izbrisana auta po pretrazi:{new_line}{new_line.join(deleted_urls_to_mail)}"
---
+++ car_notify/scraper.py
@@ -0,0 +1,33 @@
+import requests
+from lxml import html
+import helper_methods
+import smtplib
+
+url = "https://www.olx.ba/pretraga?kategorija=18&sort_order=desc&do=13100&godiste_min=2010&kilometra-a_max=131000&gorivo_select_benzin=Benzin&gorivo_select_plin=Plin&stranica="
+
+def main():
+    with open("urls.txt", 'r') as file: current_urls = file.read().splitlines()
+    new_urls = []
+
+    for i in range(helper_methods.get_number_of_pages(url)):
+        new_urls.extend(helper_methods.get_cars_urls(url + str(i+1)))
+
+    deleted_urls_to_mail = []
+    new_urls_to_mail = []
+    for value in current_urls:
+        if value not in new_urls:
+            current_urls.remove(value)
+            deleted_urls_to_mail.append(value)
+
+    for value in new_urls:
+        if value not in current_urls:
+            current_urls.append(value)
+            new_urls_to_mail.append(value)
+
+    with open("urls.txt", 'w') as file: file.write('\n'.join(current_urls))
+    email_body = helper_methods.get_email_body(new_urls_to_mail, deleted_urls_to_mail)
+    helper_methods.send_email("emir.hodzic@outlook.com", "", "emir.hodzich@gmail.com", "OLX update", email_body)
+
+if __name__ == '__main__':
+    main()
+    
