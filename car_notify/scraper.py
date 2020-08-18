5e08ddb23a8b7daa878b21216025abb22d1476b2
emiride <emir.hodzich@gmail.com>
2020-08-18 09:31:30 +0200

    added secrets

--- car_notify/scraper.py
+++ car_notify/scraper.py
@@ -2,6 +2,7 @@ import requests
 from lxml import html
 import helper_methods
 import smtplib
+import os
 
 url = "https://www.olx.ba/pretraga?kategorija=18&sort_order=desc&do=13100&godiste_min=2010&kilometra-a_max=131000&gorivo_select_benzin=Benzin&gorivo_select_plin=Plin&stranica="
 
@@ -26,8 +27,9 @@ def main():
 
     with open("urls.txt", 'w') as file: file.write('\n'.join(current_urls))
     email_body = helper_methods.get_email_body(new_urls_to_mail, deleted_urls_to_mail)
+    pwd = os.environ["OUTLOOK_PWD"]
     if len(deleted_urls_to_mail) != 0 or len(new_urls_to_mail) != 0:
-        helper_methods.send_email("emir.hodzic@outlook.com", "Masnocic@123", "emir.hodzich@gmail.com", "OLX update", email_body)
+        helper_methods.send_email("emir.hodzic@outlook.com", pwd, "emir.hodzich@gmail.com", "OLX update", email_body)
     
 if __name__ == '__main__':
     main()
